42c06d0498eb2b339a6cf259dba01e628bfe8d29
asdius <41333988+asdius@users.noreply.github.com>
2020-10-15 08:42:55 -0700

    asdius - main
    imagenes y varios

--- Assets/Scripts/Prueba/manager/plataformDetection.cs
+++ Assets/Scripts/Prueba/manager/plataformDetection.cs
@@ -7,10 +7,20 @@ public class plataformDetection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (Application.platform == RuntimePlatform.Android && gameObject.name== "instrucciones print")    //aplicado en scene draw : a -> instruction print
+        if (Application.platform == RuntimePlatform.Android)    
         {
-            Destroy(gameObject);
+            if(gameObject.name == "instrucciones print")
+            { //aplicado en scene draw : a -> instruction print
+                Destroy(gameObject);
+            }
+            else if(gameObject.name == "Lugares especiales-UI")
+            {
+                gameObject.transform.localScale = new Vector3(0.65f,0.65f,0.65f);
+                gameObject.transform.position += new Vector3(180f, 0f, 0f);
+            }
+
         }
+       // gameObject.transform.position += new Vector3(180f, 0f, 0f);
     }
 
 

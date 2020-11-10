b0d4f059d374750f27ce73e6df309320038ebf07
Andres Serna Desktop <andres.s@polygon.us>
2020-11-10 09:36:07 -0500

    Puzzle

---
+++ Assets/Scripts/Prueba/Puzzle/Puzzle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    #region Information
+    List<RectTransform> pieces;
+    #endregion
+
+    void OnEnable()
+    {
+        for (int i = 0; i < pieces.Count; i++)
+        {
+
+        }
+    }
+}

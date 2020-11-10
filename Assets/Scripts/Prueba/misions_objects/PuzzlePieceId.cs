713a5ac74967dd0bfa3921c892790a0e145c1c5a
Andres Serna Desktop <andres.s@polygon.us>
2020-11-10 09:19:36 -0500

    Puzzle

---
+++ Assets/Scripts/Prueba/misions_objects/PuzzlePieceId.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePieceId : MonoBehaviour
+{
+    #region Information
+    [Header("Information")]
+    [SerializeField]
+    int id;
+    [SerializeField]
+    bool get;
+    #endregion
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Puzzle Piece " + id.ToString()))
+            get = PlayerPrefs.GetString("Puzzle Piece " + id.ToString()) == "true";
+        else
+            PlayerPrefs.SetString("Puzzle Piece " + id.ToString(), "false");
+    }
+}

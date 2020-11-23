984739a1426fabbb59f5f47efdb746195355e607
CERBO1499 <47153483+CERBO1499@users.noreply.github.com>
2020-11-23 14:42:15 -0500

    Carlos-RayCast Correct

--- Assets/Scripts/Prueba/SelectionAge/ManagerGameAge.cs
+++ Assets/Scripts/Prueba/SelectionAge/ManagerGameAge.cs
@@ -7,6 +7,7 @@ public class ManagerGameAge : MonoBehaviour
     #region Information
     [Header("Pines position")]
     [SerializeField] List<GameObject> Pines;
-
+    [Header("Canvas Pines")]
+    [SerializeField] List<GameObject> CanvasPines;
     #endregion
 }

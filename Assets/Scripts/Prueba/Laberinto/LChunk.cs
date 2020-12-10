e08da463a72348d4e9caa65024497d7a1cca1504
Polyxeivol <andres.@polygon.us>
2020-12-10 11:50:19 -0500

    Laberinto Chunk

---
+++ Assets/Scripts/Prueba/Laberinto/LChunk.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LChunk : MonoBehaviour
+{
+    #region Information
+    [Header("Information")]
+    [SerializeField] GameObject nextChuck;
+    [SerializeField] Vector2 localPosition;
+
+    public static new bool enabled;
+    #endregion
+
+    #region Components
+    [Header("Components")]
+    [SerializeField] RectTransform player;
+    #endregion
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        enabled = false;
+
+        foreach (RectTransform rect in nextChuck.GetComponent<RectTransform>().parent.gameObject.GetComponent<RectTransform>())
+        {
+            if (rect.gameObject.activeSelf)
+            {
+                rect.gameObject.SetActive(false);
+
+                break;
+            }
+        }
+
+        nextChuck.SetActive(true);
+
+        player.localPosition = localPosition;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        enabled = true;
+    }
+}

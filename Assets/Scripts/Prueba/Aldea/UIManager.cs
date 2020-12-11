8b01247d0a3c5b520d00096aa5662885710cf0dd
Polyxeivol <andres.@polygon.us>
2020-12-11 18:14:21 -0500

    Circo

---
+++ Assets/Scripts/Prueba/Aldea/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    #region Information
+    [Header("Information")]
+    #region Parts
+    [Header("Parts")]
+    [SerializeField] RectTransform circus;
+    #endregion
+    [Space]
+    [SerializeField] GameObject introduction;
+    System.Action Pass;
+    #endregion
+
+    public void ActiveFirstactivity(GameObject firstActivity)
+    {
+        introduction.SetActive(false);
+
+        firstActivity.SetActive(true);
+
+        Pass = () => 
+        {
+            firstActivity.SetActive(false);
+        };
+    }
+
+    public void PlayFirstactivity(GameObject firstActivity)
+    {
+        Pass();
+
+        firstActivity.SetActive(true);
+
+        Pass = () =>
+        {
+            firstActivity.SetActive(false);
+        };
+    }
+}
---
+++ Assets/Scripts/Prueba/Aldea/Word.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Word : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    #region Information
+    Vector2 initialLocalPosition;
+    int initialSiblingIndex;
+    #region Drag
+    int box;
+    RectTransform keeper;
+    Coroutine dragCorotuine;
+    #endregion
+    #endregion
+
+    #region Components
+    RectTransform rect;
+    #endregion
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+
+        initialLocalPosition = rect.localPosition;
+
+        initialSiblingIndex = rect.GetSiblingIndex();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragCorotuine = StartCoroutine(DragCoroutine());
+
+        rect.SetSiblingIndex(rect.parent.childCount - 1);
+    }
+
+    IEnumerator DragCoroutine()
+    {
+        while (true)
+        {
+            Vector3 screenPoint = Input.mousePosition;
+
+            screenPoint.z = 100f;
+
+            rect.position = Camera.main.ScreenToWorldPoint(screenPoint);
+
+            yield return null;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        StopCoroutine(dragCorotuine);
+
+        dragCorotuine = null;
+
+        if (keeper != null)
+            rect.localPosition = keeper.localPosition;
+        else
+            rect.localPosition = initialLocalPosition;
+
+        rect.SetSiblingIndex(initialSiblingIndex);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "keeper")
+        {
+            box++;
+
+            keeper = collision.gameObject.GetComponent<RectTransform>();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "keeper")
+        {
+            box--;
+
+            if(box == 0)
+                keeper = null;
+        }
+    }
+}

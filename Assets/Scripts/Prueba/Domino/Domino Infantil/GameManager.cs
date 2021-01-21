f51358056091c9b1c67026a6d4ef0bafb0eb752f
Polyxeivol <andres.@polygon.us>
2021-01-21 10:09:35 -0500

    Diverdomino

--- Assets/Scripts/Prueba/Domino/Domino Infantil/GameManager.cs
+++ Assets/Scripts/Prueba/Domino/Domino Infantil/GameManager.cs
@@ -2,34 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace Diverdomino
 {
-    #region Information
-    [SerializeField] List <PieceDomino> piecesToDistribute; 
-    [SerializeField] List <PieceDomino> piecesToPlayer;
-    [SerializeField] List <PieceDomino> piecesToMachine;
-    [SerializeField] RectTransform [] positions;
-    #endregion
-    
-
-    private void Awake()
+    public class GameManager : MonoBehaviour
     {
-        DistributePiecesAleatori();
-    }
-    void DistributePiecesAleatori()
-    {        
-        for (int i = 0; i < 14; i++)
+        #region Static
+        public static GameManager instance;
+        #endregion
+
+        #region Information
+        public bool drag = true;
+        [SerializeField] List<PieceDomino> piecesToDistribute;
+        [SerializeField] List<PieceDomino> piecesToPlayer;
+        [SerializeField] List<PieceDomino> piecesToMachine;
+        [SerializeField] RectTransform[] positions;
+        #endregion
+
+
+        private void Awake()
         {
-            PieceDomino myPiece = piecesToDistribute[Random.Range(0, piecesToDistribute.Count)];
-            piecesToDistribute.Remove(myPiece);
-            piecesToPlayer.Add(myPiece);            
-            myPiece.transform.position = positions[i].transform.position; 
+            instance = this;
+
+            DistributePiecesAleatori();
         }
-        for (int i = 0; i < 14; i++)
+        void DistributePiecesAleatori()
         {
-            PieceDomino myPiece = piecesToDistribute[Random.Range(0, piecesToDistribute.Count)];
-            piecesToDistribute.Remove(myPiece);
-            piecesToMachine.Add(myPiece);
+            for (int i = 0; i < 14; i++)
+            {
+                PieceDomino myPiece = piecesToDistribute[Random.Range(0, piecesToDistribute.Count)];
+                piecesToDistribute.Remove(myPiece);
+                piecesToPlayer.Add(myPiece);
+                myPiece.transform.position = positions[i].transform.position;
+            }
+            for (int i = 0; i < 14; i++)
+            {
+                PieceDomino myPiece = piecesToDistribute[Random.Range(0, piecesToDistribute.Count)];
+                piecesToDistribute.Remove(myPiece);
+                piecesToMachine.Add(myPiece);
+            }
         }
     }
 }
--- Assets/Scripts/Prueba/Domino/Domino Infantil/PieceDomino.cs
+++ Assets/Scripts/Prueba/Domino/Domino Infantil/PieceDomino.cs
@@ -1,10 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class PieceDomino : MonoBehaviour
+public class PieceDomino : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler
 {
+    #region Components
     RectTransform rectTrasnform;
-
     public RectTransform PrectTrasnform { get => rectTrasnform; set => rectTrasnform = value; }
+    Image img;
+    #endregion
+
+
+    void Awake()
+    {
+        rectTrasnform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        img.color = new Color(1f, 1f, 1f, 0.5f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+         img.color =Color.white;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {/*
+        if (drag)
+        {
+            lastIndex = rect.GetSiblingIndex();
+
+            rect.SetParent(rect.parent.parent.parent.parent);
+
+            root.sizeDelta = new Vector3(root.sizeDelta.x, root.sizeDelta.y - (rect.sizeDelta.y + 240));
+
+            StartCoroutine(DragCoroutine(eventData));
+        }*/
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
 }
